f07f0127b6e798b0fce33853a3fe35affeb29be8
Jaydeep Sathvara <119073219+jaydeep1124@users.noreply.github.com>
2023-02-23 01:27:07 -0500

    Update SelectButton.js

--- src/components/SelectButton.js
+++ src/components/SelectButton.js
@@ -3,22 +3,21 @@ import { makeStyles } from "@material-ui/core";
 const SelectButton = ({ children, selected, onClick }) => {
   const useStyles = makeStyles({
     selectbutton: {
-      border: "1px solid gold",
+      border: "1px solid #FFFFFF",
       borderRadius: 5,
       padding: 10,
       paddingLeft: 20,
       paddingRight: 20,
       fontFamily: "Montserrat",
       cursor: "pointer",
-      backgroundColor: selected ? "gold" : "",
-      color: selected ? "black" : "",
+      backgroundColor: selected ? "#FFFFFF" : "transparent",
+      color: selected ? "#333333" : "#FFFFFF",
       fontWeight: selected ? 700 : 500,
       "&:hover": {
-        backgroundColor: "gold",
-        color: "black",
+        backgroundColor: "#FFFFFF",
+        color: "#333333",
       },
       width: "22%",
-      //   margin: 5,
     },
   });
 
