694690543c536f0eb6d97891ac0f26a4b54de1aa
Jaydeep Sathvara <119073219+jaydeep1124@users.noreply.github.com>
2023-02-23 01:39:30 -0500

    Update CoinsTable.js

--- src/components/CoinsTable.js
+++ src/components/CoinsTable.js
@@ -21,33 +21,33 @@ import { CoinList } from "../config/api";
 import { useHistory } from "react-router-dom";
 import { CryptoState } from "../CryptoContext";
 
-const numberWithCommas = (x) => {
+export function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-};
+}
 
-const useStyles = makeStyles({
-  row: {
-    backgroundColor: "#16171a",
-    cursor: "pointer",
-    "&:hover": {
-      backgroundColor: "#131111",
-    },
-    fontFamily: "Montserrat",
-  },
-  pagination: {
-    "& .MuiPaginationItem-root": {
-      color: "gold",
-    },
-  },
-});
-
-const CoinsTable = () => {
+export default function CoinsTable() {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(false);
   const [search, setSearch] = useState("");
   const [page, setPage] = useState(1);
 
   const { currency, symbol } = CryptoState();
+
+  const useStyles = makeStyles({
+    row: {
+      backgroundColor: "#16171a",
+      cursor: "pointer",
+      "&:hover": {
+        backgroundColor: "#131111",
+      },
+      fontFamily: "Montserrat",
+    },
+    pagination: {
+      "& .MuiPaginationItem-root": {
+        color: "gold",
+      },
+    },
+  });
 
   const classes = useStyles();
   const history = useHistory();
@@ -72,13 +72,14 @@ const CoinsTable = () => {
 
   useEffect(() => {
     fetchCoins();
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currency]);
 
   const handleSearch = () => {
     return coins.filter(
       (coin) =>
-        coin.name.toLowerCase().includes(search.toLowerCase()) ||
-        coin.symbol.toLowerCase().includes(search.toLowerCase())
+        coin.name.toLowerCase().includes(search) ||
+        coin.symbol.toLowerCase().includes(search)
     );
   };
 
