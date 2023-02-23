432eb026c48fb4e8698c4e8e2c40563265531df4
Jaydeep Sathvara <119073219+jaydeep1124@users.noreply.github.com>
2023-02-23 01:12:43 -0500

    Update CryptoContext.js

--- src/CryptoContext.js
+++ src/CryptoContext.js
@@ -3,8 +3,8 @@ import React, { createContext, useContext, useEffect, useState } from "react";
 const Crypto = createContext();
 
 const CryptoContext = ({ children }) => {
-  const [currency, setCurrency] = useState("INR");
-  const [symbol, setSymbol] = useState("₹");
+  const [currency, setCurrency] = useState("EUR");
+  const [symbol, setSymbol] = useState("€");
 
   useEffect(() => {
     if (currency === "EUR") setSymbol("€");
